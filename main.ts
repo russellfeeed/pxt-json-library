868338dca0f9730d15388d54df9729cfbcfc38bf
russellfeeed <russell@feeed.com>
2019-10-20 15:59:14 +0100

    Added namespace annotations

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-namespace jsonLibrary {
+/**
+ * Provides access to JSON micro:bit functionality.
+ * Useful for constructing and sending data over HTTP
+ * using something like wifi:bit
+ */
+//% color=190 weight=100 icon="\uf1ec" block="JSON Blocks"
+namespace jsonBlocks {
 
     //% block
     export function WriteJSON() {
